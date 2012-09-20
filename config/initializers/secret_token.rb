7fcf6ee33187f1f71272b111a7d5d144b44e2640
Brad Wrage <bradwrage@sjc-vpn3-1288.cisco.com>
2012-09-19 20:45:49 -0700

    Created treebook app and status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'e8267d2eb01289729f1fba289f54976a73a09100e69fbd88a2c9eda3cfa46cb2c9c198f96a190f06e6c39c0538e896a9b6344f29c81bca7810ceb19cec4ad7ac'
